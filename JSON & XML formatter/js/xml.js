d5d2b5e0498a411e78fe015ea966adb8cfa31ddf
Souradip Ghosh <souradip1212@gmail.com>
2021-07-25 20:30:49 +0530

    Update xml.js

--- JSON & XML formatter/js/xml.js
+++ JSON & XML formatter/js/xml.js
@@ -2,15 +2,24 @@ function processXML(){
     try {
         $("#xmlValidatorFieldOutput").empty();
         var xml = $("#xmlValidatorField").val().trim();
-        xml = hljs.highlight(xml, {language: 'xml'});
-        //console.log(xml);
-        $("#xmlValidatorFieldOutput").html(xml.value);
-        document.querySelectorAll('#xmlValidatorFieldOutput').forEach((el) => {
-            hljs.highlightElement(el);
-        });
-
-       // $("#xmlValidatorFieldOutput").simpleXML({ xmlString: xml });
-        $("#xmlMessage").html('<div class="ui positive message transition successMessagebackground">Valid XML</div>');
+        var parser = new DOMParser();
+        var outputVal = parser.parseFromString(xml,"text/xml");
+       // console.log(outputVal);
+        var find = $(outputVal).find("parsererror").length;
+        if(find == 0){
+            xml = hljs.highlight(xml, {language: 'xml'});
+            //console.log(xml);
+            $("#xmlValidatorFieldOutput").html(xml.value);
+            document.querySelectorAll('#xmlValidatorFieldOutput').forEach((el) => {
+                hljs.highlightElement(el);
+            });
+    
+           // $("#xmlValidatorFieldOutput").simpleXML({ xmlString: xml });
+            $("#xmlMessage").html('<div class="ui positive message transition successMessagebackground">Valid XML</div>');
+        }else{
+            $("#jsonXmlMessage").html('<div class="ui negative message transition errorMessagebackground">Invalid XML</div>');
+        }
+        
        
     } catch (ex) {
         $("#xmlMessage").html('<div class="ui negative message transition errorMessagebackground">'+ex+'</div>');
